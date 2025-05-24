81c9bdb1e7a9b1af589af3e3986af58bda4f5c56
Dare-88-commit <daysem120@gmail.com>
2025-05-24 20:23:55 +0100

    (week_11)

---
+++ practice_folder/.py
@@ -0,0 +1,14 @@
+x = 10
+
+
+def outer_func():
+    x = 20
+
+    def inner_func():
+        global x
+        x = 30
+    inner_func()
+    print("x in outer_func:", x)
+
+
+print("x in global:", x)
